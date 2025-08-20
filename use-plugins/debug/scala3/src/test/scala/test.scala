48c8ad0103aae75c2c8d14b37e4a62292cb6f086
Aditya Naik <aditya.naik@sifive.com>
2025-08-20 14:32:58 -0700

    Cleanup and add comment

--- use-plugins/debug/scala3/src/test/scala/test.scala
+++ use-plugins/debug/scala3/src/test/scala/test.scala
@@ -3,10 +3,6 @@ package debug
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class One {
-  val one = 1
-}
-
 abstract class Okable { // Record
   def _doNothing: Unit = println("Nothing")
 } 
@@ -16,6 +12,7 @@ abstract class SomeClass extends Okable // Bundle
 class SomeChildClass extends SomeClass // BadSeqBundle
 
 class WrapperSpec extends AnyFlatSpec with Matchers {
+  // Removing Matchers mixin & changing this to "Wrapping types" { works fine
   "Wrapping types" should "work" in {
     val sc = new SomeClass {
       val scc = new SomeChildClass
@@ -24,6 +21,5 @@ class WrapperSpec extends AnyFlatSpec with Matchers {
 }
 
 @main def run() = {
-  println("running")
   val z = new WrapperSpec
 }
