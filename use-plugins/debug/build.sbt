1e7286605133828a753cc0c8189a567e6ccf01de
Aditya Naik <aditya.naik@sifive.com>
2025-08-20 14:26:15 -0700

    Minimize issue

--- use-plugins/debug/build.sbt
+++ use-plugins/debug/build.sbt
@@ -15,7 +15,7 @@ lazy val debugPluginLocation = (project in file("scala3"))
     name := "Using the debug plugin for Scala 3.",
     scalaVersion := scala3Version,
     autoCompilerPlugins := true,
-    addCompilerPlugin("debug-plugin-org" % "debug" % "0.0.1-SNAPSHOT"),
+    addCompilerPlugin("debug-plugin-org" % "debug_3" % "0.0.1-SNAPSHOT"),
     scalacOptions := Seq("-Vprint:pluginDebugPhase"),
     libraryDependencies ++= Seq(
       "org.scalatest" %% "scalatest" % "3.2.19" % "test"
--- use-plugins/debug/scala3/src/test/scala/test.scala
+++ use-plugins/debug/scala3/src/test/scala/test.scala
@@ -13,32 +13,15 @@ abstract class SomeClass extends Okable // Bundle
 
 class SomeChildClass extends SomeClass // BadSeqBundle
 
-class Container // Module
-
 object ChildContainer { // IO
   def apply[T](gen: => T): T = gen
 }
 
-def wrapperFn(some: => Any): Unit = { // thrownBy
-  println("wrapping thing")
-}
-
-def emitThing(gen: => Container): Unit = { // emitChirrtl
-  val fn = () => gen
-  println("emitting thing")
-}
-
 class WrapperSpec extends AnyFlatSpec with Matchers {
   "Wrapping types" should "work" in {
-    wrapperFn {
-      emitThing {
-        new Container {
-          val sc = ChildContainer(new SomeClass {
-            val scc = new SomeChildClass
-          })
-        }
-      }
-    }
+    val sc = ChildContainer(new SomeClass {
+      val scc = new SomeChildClass
+    })
   }
 }
 
